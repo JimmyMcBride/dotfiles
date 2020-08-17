dc3b5ce2ddd590e9ccb46354c89a9def14502ca4
JimmyMcBride <mcbride967@gmail.com>
2020-08-17 00:07:17 -0500

    update

--- script.sh
+++ script.sh
@@ -16,6 +16,8 @@ git clone https://github.com/jimeh/tmux-themepack.git ~/.tmux-themepack
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
+source ~/.bashrc
+
 nvm install node
 
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
@@ -24,13 +26,25 @@ cat bashrc >> .bashrc
 
 rm tmux.conf bashrc vimrc
 
-sudo apt-get update
+sudo apt-get update -y
 
-sudo apt-get upgrade
+sudo apt-get upgrade -y
 
-sudo apt install --no-install-recommends yarn
+sudo apt install --no-install-recommends yarn -y
 
-sudo apt-get install tmux neovim pm2 postgresql postgresql-contrib nginx docker.io
+sudo apt-get install tmux -y
+
+sudo apt-get install neovim -y
+
+sudo apt-get install postgresql -y
+
+sudo apt-get install postgresql-contrib -y
+
+sudo apt-get install nginx -y
+
+sudo apt-get install docker.io -y
+
+npm i -g pm2
 
 sudo systemctl start nginx
 
